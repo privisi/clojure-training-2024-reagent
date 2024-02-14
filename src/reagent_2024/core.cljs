a9d4084712e911518b0eb4a1035a19d371422342
Privisi <BZhang705@hotmail.com>
2024-02-14 23:40:38 +0900

    Form-2 components

--- src/reagent_2024/core.cljs
+++ src/reagent_2024/core.cljs
@@ -2,7 +2,7 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
    [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]
+   [reagent.core :as reagent]
    [reagent.dom :as rdom]
    [cljs.core.async :refer [<! >! chan timeout]]))
 
@@ -10,10 +10,61 @@
 ;; which can sometimes be useful.
 (enable-console-print!)
 
-;;; Integrating Reagent with core.async.
+;;; A bit more Reagent: form-1 vs form-2 components
+;;
+;;  Good doc at: https://cljdoc.org/d/reagent/reagent/1.0.0-alpha2/doc/documentation-index
+;; https://cljdoc.org/d/reagent/reagent/1.0.0-alpha2/doc/tutorials/creating-reagent-components?q=form#form-1-a-simple-function
+;; https://cljdoc.org/d/reagent/reagent/1.0.0-alpha2/doc/tutorials/creating-reagent-components?q=form#form-2--a-function-returning-a-function
+;; Sometimes our components want to maintain their own local state.
+
+;; Consider this one:
+(defn timer-component [label update-frequency]
+  (let [seconds-elapsed (reagent/atom 0)] ;; setup, and local state
+    (fn [label update-frequency]          ;; inner, render function is returned
+      (go
+        (<! (timeout (* 1000 update-frequency)))
+        (swap! seconds-elapsed + update-frequency))
+      [:div
+       "Component: " label
+       ": seconds Elapsed: " @seconds-elapsed])))
+
+;; Each new component, displayed in [timer-component] will have their own
+;; local atom, and local argument to the function, independent of each other.
+
+;;;; Cursors
+
+;; First create a ratom
+(def state (reagent/atom {:foo {:bar "BAR"}
+                          :baz "BAZ"
+                          :quux "QUUX"}))
+
+;; Now create a cursor
+(def bar-cursor (reagent/cursor state [:foo :bar]))
+
+(defn state-component []
+  (println "Redisplaying STATE")
+  [:div "State: " @state])
+
+(defn quux-component []
+  (println "Redisplaying QUUX")
+  [:div (:quux @state)])
+
+(defn bar-component []
+  (println "Redisplaying BAR")
+  [:div @bar-cursor])
+
+;; Now if only a partial part of the state mutates: (watch the console log)
+(comment
+  (swap! state assoc-in [:foo :bar] "The new bar")
+
+  (swap! state assoc :baz "A new key appears"))
+
+
+;; This idea will be used (implicitly) by the re-frame framework
+;; which we'll study next class.
 
 ;; Let us declare our state globally:
-(defonce the-counter (atom 0))
+(defonce the-counter (reagent/atom 0))
 
 ;; Now let us create a communication channel which our
 ;; UI can sent event to.
@@ -45,6 +96,12 @@
 (defn simple-button []
     [:div
      [:center
+      [:h1 "Timer component"]
+      [timer-component "Fast" 1]
+      [timer-component "Slow" 3]
+      [state-component]
+      [quux-component]
+      [bar-component]
       [:h1 "Simple button example"]
       [:input {:type :button
                :class :button
